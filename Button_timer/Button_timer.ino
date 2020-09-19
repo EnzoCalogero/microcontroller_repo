e2afb3490a03cb591a593c6f7411defbff35f206
enzo calogero <enzus@hotmail.com>
2020-09-19 23:14:31 +0100

    second

--- Button_timer/Button_timer.ino
+++ Button_timer/Button_timer.ino
@@ -9,6 +9,20 @@ const int time_on = 10000;     // time to keep the system up
 // variables will change:
 int buttonState = 0;         // variable for reading the pushbutton status
 
+void light_routine() {
+    digitalWrite(ledPin0, HIGH);
+    digitalWrite(ledPin3, HIGH);
+    digitalWrite(ledPin4, HIGH);
+    digitalWrite(ledPin1, HIGH);
+    delay(time_on);
+    
+    // turn LED off:
+    digitalWrite(ledPin0, LOW);
+    digitalWrite(ledPin3, LOW);
+    digitalWrite(ledPin4, LOW);
+    digitalWrite(ledPin1, LOW);
+}
+
 void setup() {
   // initialize the LED pin as an output:
   pinMode(ledPin0, OUTPUT);
@@ -17,17 +31,10 @@ void setup() {
   pinMode(ledPin1, OUTPUT);
   // initialize the pushbutton pin as an input:
   pinMode(buttonPin, INPUT);
-  digitalWrite(ledPin0, HIGH);
-    digitalWrite(ledPin3, HIGH);
-    digitalWrite(ledPin4, HIGH);
-    digitalWrite(ledPin1, HIGH);
-  delay(time_on);
-  // turn LED off:
-  digitalWrite(ledPin0, LOW);
-    digitalWrite(ledPin3, LOW);
-    digitalWrite(ledPin4, LOW);
-    digitalWrite(ledPin1, LOW);
-    
+
+
+  light_routine();
+   
 }
 
 void loop() {
@@ -35,18 +42,7 @@ void loop() {
   buttonState = digitalRead(buttonPin);
   // check if the pushbutton is pressed. If it is, the buttonState is HIGH:
   if (buttonState == HIGH) {
-    // turn LED on:
-    digitalWrite(ledPin0, HIGH);
-    digitalWrite(ledPin3, HIGH);
-    digitalWrite(ledPin4, HIGH);
-    digitalWrite(ledPin1, HIGH);
-    delay(time_on);
-    // turn LED off:
-    //delay(500);
-    digitalWrite(ledPin0, LOW);
-    digitalWrite(ledPin3, LOW);
-    digitalWrite(ledPin4, LOW);
-    digitalWrite(ledPin1, LOW);
-    
+    // turn LEDs on:
+    light_routine();
     }
 }
