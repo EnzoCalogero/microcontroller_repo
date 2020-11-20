c603fab298b79e09684a9bdf47cd16b0b0a7d741
enzo calogero <enzus@hotmail.com>
2020-11-20 14:22:00 +0000

    temporizzatore

--- attiny85/temporizatore/temporizzatore/temporizzatore.ino
+++ attiny85/temporizatore/temporizzatore/temporizzatore.ino
@@ -10,13 +10,11 @@ const int voice =  4;         // the number of the LED pin
 // variables will change:
 int buttonState = 0;         // variable for reading the pushbutton status
 
-
 void start_end() {
   digitalWrite(ledPin3, HIGH);;
   delay(500);
   digitalWrite(ledPin3, LOW);
   }
-
 
 void routine_hands() {
   
@@ -25,6 +23,7 @@ void routine_hands() {
     // turn LED off:
     for (int i = 0; i <= 20; i++) {
       digitalWrite(ledPin3, HIGH);;
+      
       delay(50);
       digitalWrite(ledPin3, LOW);
       delay(950);
@@ -37,17 +36,18 @@ void routine_hands() {
 void routine_teeth() {
     digitalWrite(ledPin0, HIGH);
     start_end();
+    
     for (int i = 0; i <= 20; i++) {
       digitalWrite(ledPin3, HIGH);;
       delay(50);
       digitalWrite(ledPin3, LOW);
-      delay(6950);
+      delay(5900); //50 millisecond discount....
       } 
+    
     
     // turn LED off:
     digitalWrite(ledPin0, LOW);
     start_end();
-    //digitalWrite(ledPin3, LOW);
     }
 
     
@@ -61,7 +61,7 @@ void setup() {
   // initialize the pushbutton pin as an input:
     pinMode(buttonPin1, INPUT);
     pinMode(buttonPin2, INPUT);
-
+    
     // Routine Startup Iniziale
     digitalWrite(ledPin3, HIGH);
     
@@ -80,6 +80,7 @@ void loop() {
     if (buttonState == HIGH) {
             routine_hands();
             }
+
     buttonState = digitalRead(buttonPin2);
     // check if the pushbutton is pressed. If it is, the buttonState is HIGH:
     if (buttonState == HIGH) {
