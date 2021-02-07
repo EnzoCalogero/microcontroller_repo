3af44d6efc747bcf5422384a83f7994ea33e468a
enzo calogero <enzus@hotmail.com>
2021-02-07 17:39:49 +0100

    2021_02_07

---
+++ arduino/solenoid/solenoid/solenoid.ino
@@ -0,0 +1,14 @@
+int solenoidPin = 9;                    //This is the output pin on the Arduino
+
+void setup() 
+{
+  pinMode(solenoidPin, OUTPUT);          //Sets that pin as an output
+}
+
+void loop() 
+{
+  digitalWrite(solenoidPin, HIGH);      //Switch Solenoid ON
+  delay(50);                          //Wait 1 Second
+  digitalWrite(solenoidPin, LOW);       //Switch Solenoid OFF
+  delay(100);                          //Wait 1 Second
+}
